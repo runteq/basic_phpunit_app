18f9f7642e43e7bb293d7ac4a327be01877b6eaf
majestictree <unlimited.blade.works22@gmail.com>
2025-02-18 18:30:20 +0900

    fix: isValid()関数の追加

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,21 +3,47 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class Task extends Model
 {
     protected $fillable = [
-        'title', 'content', 'status', 'priority', 'deadline',
+        'title', 'content', 'status', 'priority', 'deadline', 'user_id',
     ];
+
+    protected $casts = [
+        'status'   => 'integer',
+        'deadline' => 'datetime',
+    ];
+
+    protected $errors;
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
-    protected $casts = [
-        'status' => 'integer',
-        'deadline' => 'datetime',
-    ];
+    public function isValid()
+    {
+        $rules = [
+            'title'  => 'required|unique:tasks,title,' . $this->id,
+            'status' => 'required|integer',
+        ];
+
+        $validator = Validator::make($this->attributes, $rules);
+
+        if ($validator->fails()) {
+            $this->errors = $validator->errors();
+            return false;
+        }
+
+        $this->errors = collect();
+        return true;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 }
 
